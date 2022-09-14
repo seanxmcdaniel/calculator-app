a07a8f4dda7d85e0794c100f93beedc1522b8af6
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-09-14 00:38:30 -0400

    Styling the calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("calc_form");
+const output = document.getElementById("output");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+let is_operator = false;
+operand_btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (output.value == "0") {
+            output.value = e.target.value;
+        } else if (output.value.includes(".")) {
+            output.value = output.value + "" + e.target.value.replace(".", "");
+        } else if (is_operator) {
+            output.value = e.target.value;
+        } else {
+            output.value = output.value + "" + e.target.value;
+        }
+    })
+})
+
+
